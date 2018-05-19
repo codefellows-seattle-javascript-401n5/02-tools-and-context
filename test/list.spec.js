6d7d5b052676bb24ddb405f06ca9e8668e11ddaf
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-05-19 14:30:02 -0700

    pop

--- test/list.spec.js
+++ test/list.spec.js
@@ -7,14 +7,45 @@ describe('List Modules', () => {
   it('push(), returns the updated length of array', () => {
 
     let myList = new List();
-    myList.push('foo');
+    myList.push('one');
 
     expect(myList.length).toBe(1);
 
   });
   it('push(), adds an element to the list', () => {
-
+    let myList = new List();
+    myList.push('one');
+    // myList.push('two');
     
+    expect(myList[myList.length - 1]).toEqual('one');
+    
+  });
+
+  it('pop() should return undefined if the list is empty', () => {
+
+    let myList = new List();
+
+    expect(myList.pop()).toBeUndefined();
+  });
+
+  it('pop(), removes the last element of the list', () => {
+
+    let myList = new List();
+    myList.push('one');
+    myList.push('two');
+
+    expect(myList.pop()).toEqual('two');
+
+  });
+
+  it('pop(), returns the element to the list', () => {
+
+    let myList = new List();
+    myList.push('one');
+    myList.push('two');
+
+    expect(myList.pop()).toEqual('two');
+
   });
 });
 
