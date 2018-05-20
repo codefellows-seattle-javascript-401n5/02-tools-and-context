6996e6bc7445146962f039e6ae906660d64a94c7
annaboyatyuk <annaboyatyuk@gmail.com>
2018-05-19 18:22:15 -0700

    for each function

--- lab-anna/__test__/lib/list.spec.js
+++ lab-anna/__test__/lib/list.spec.js
@@ -7,15 +7,67 @@ describe('List Module', () => {
   it('push() returns the length of the list', () => {
 
     let myList = new List();
+    
     myList.push('FOO');
 
     expect(myList.length).toBe(1);
   });
 
-  it('push() adds an element to a list', () => {
+  it('push() adds an element to the end of a list', () => {
 
+    let myList = new List();
+    myList.push('FOO');
 
-    
+    expect(myList[0]).toBe('FOO');
   });
 
+  it('pop() should return undefined if the list is empty', () => {
+
+    let myList = new List();
+
+    expect(myList.pop()).toBeUndefined();
+  });
+
+
+  it('pop() removes an element from the end of a list', () => {
+
+    let myList = new List();
+    myList.push('a');
+    myList.push('b');
+
+    expect(myList.pop()).toBe('b');
+  });
+
+  it('forEach() function should return undefined', () => {
+
+    let myList = new List();
+    myList.push('aa');
+    myList.push('bb');
+
+    expect(myList.forEach(ele => {
+      undefined;
+      return ele;
+    })).toEqual(undefined);
+  });
+
+  it('forEach() array should be unchanged', () => {
+
+    let myList = new List();
+    let arr2 = new List;
+    
+    myList.push('aa');
+    myList.push('be');
+    myList.forEach(ele => {
+      arr2.push(ele + 'e');
+    });
+    
+
+    expect(arr2[1]).toEqual('bee');
+  });
+
+
+
 });
+
+
+
--- lab-anna/lib/list.js
+++ lab-anna/lib/list.js
@@ -11,6 +11,29 @@ class List {
     return this.length;
   }
 
+  pop() {
+    let remove = this[this.length - 1];
+
+    this.length--;
+
+    if(this.length < 0) {
+      return undefined;
+    }
+    else {
+      delete this[this.length];
+    }
+
+    return remove;
+  }
+
+
+  forEach(func) {
+    for(let i = 0; i < this.length; i++) {
+      func(this[i]);
+    }
+    return undefined;
+  }
+
 }
 
 // this[++this.length] = item;
