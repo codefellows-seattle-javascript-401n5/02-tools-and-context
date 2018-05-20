f7adf7ef55b05f20f662f095957baf08eac22ae2
Haley Mendoza <mendozahaley@gmail.com>
2018-05-19 21:11:08 -0700

    Pop method.

--- lab-haley/__test__/list.test.js
+++ lab-haley/__test__/list.test.js
@@ -7,14 +7,38 @@ describe('list module', () => {
   it('push should return the updated length of the list', () =>{
 
     myList.push('FOO');
+    myList.push('BAR');
 
-    expect(myList.length).toBe(1);
+    expect(myList.length).toBe(2);
   });
 
   it('push should add an element to the end of the list', () => {
-
     myList.push('BAZ');
 
-    expect(myList[myList.length - 1]).toBe('BAZ');
+    expect(myList[myList.length -1]).toBe('BAZ');
+  });
+
+  it('pop should return undefined if length is 0', () => {
+    
+    myList.length = 0;
+    expect(myList.pop()).toBeUndefined;
+
+  });
+
+  it('pop should remove the last element of the list', () => {
+    myList.push('FOO');
+    myList.push('BAR');
+    myList.push('BAZ');
+    myList.pop();
+
+    expect(myList.length).toBe(2);
+  });
+
+  it('pop should return the removed element', () => {
+    myList.push('FOO');
+    myList.push('BAR');
+    myList.push('BAZ');
+
+    expect(myList.pop()).toBe('BAZ');
   });
 });
--- lab-haley/lib/list.js
+++ lab-haley/lib/list.js
@@ -9,6 +9,26 @@ class List{
     this[this.length++] = item;
     return this.length;
   }
+
+  pop(){
+    if(!this.length){
+      return undefined;
+    }
+    const toRemove = this[this.length-1];
+    delete this[this.length-1];
+    this[this.length--];
+    return toRemove;
+  }
+
+  // pop() {
+  //   let remove = this[this.length - 1];
+  //   this.length--;
+  //   if (this.length < 0) return undefined;
+  //   else {
+  //     delete this[this.length];
+  //   }
+  //   return remove;
+  // }
 }
 
 module.exports = List;
