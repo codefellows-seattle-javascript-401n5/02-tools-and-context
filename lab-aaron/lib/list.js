87d9b70edc3da512606c5f587798bf9b177fc330
Aaron <aaronmeade9292@gmail.com>
2018-05-21 18:10:24 -0700

    fixed formatting errors

--- lab-aaron/lib/list.js
+++ lab-aaron/lib/list.js
@@ -1,34 +1,33 @@
-'use strict'
+"use strict";
 
 class List {
-    constructor(){
-        this.length = 0;
+  constructor() {
+    this.length = 0;
+  }
 
-    }
+  push(item) {
+    this[this.length] = item;
+    this.length++;
+    return this.length;
+  }
 
-    push(item){
-        this[this.length] = item;
-        this.length++;
-        return this.length;
-    }
+  pop(item) {
+    this.length--;
+    return this.length;
+  }
 
-    pop(item){
-        this.length--;
-        return this.length;
-    }
+  filter(item) {
+    return this.length;
+    return newList;
+    console.log(newList);
+  }
 
-    filter(item){
-        return this.length;
-        return newList;
-        console.log(newList);
-    }
+  reduce(item) {
+    return item;
+  }
 
-    reduce(item){
-        return item;
-    }
-
-    forEach(item){
-        return item
-    }
+  forEach(item) {
+    return item;
+  }
 }
 module.exports = List;
