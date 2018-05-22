86a700c624992206503b889283378ba9fa0d0ac9
SeatownAC <33205876+SeatownAC@users.noreply.github.com>
2018-05-21 18:26:55 -0700

    trying to get travis working via json package

--- lab-autumn/__test__/list.test.js
+++ lab-autumn/__test__/list.test.js
@@ -7,14 +7,55 @@ describe('List Module', () => {
   it('push() returns the updated length of the list', () => {
 
     let myList = new List();
-    myList.push('FOO');
+    myList.push('puppies');
 
     expect(myList.length).toBe(1);
       
   });
 
-  it('push() adds an element to the list', () => {
+  it('push() adds an element to the end of a list', () => {
 
+    let myList = new List();
+    myList.push('puppies');
+
+    expect(myList[0]).toBe('puppies');
+  });
+  it('pop() should return undefined if the list is empty', () => {
+
+    let myList = new List();
+
+    expect(myList.pop()).toBeUndefined();
+  });
+  it('pop() removes last element in list', () => {
+
+    let myList = new List();
+    myList.push('puppies');
+    myList.push('kittens');
+
+    expect(myList.pop()).toBe('puppies');
   });
 
+  it('pop() puts back the last element in the array', () => {
+    let myList = new List(); 
+    myList.push('puppies');
+    myList.push('kittens');
+
+    expect(myList.pop()).toBe('kittens');
+  });
+  it('forEach should return undefined', () => {
+    let myList = new List();
+    myList.push('puppies');
+    myList.push('kittens');
+    myList.push('monkeys');
+
+    expect(myList.forEach()).toBeUndefined();
+
+  });
+  
+
+  
+
 });
+
+'use strict';
+
--- lab-autumn/lib/list.js
+++ lab-autumn/lib/list.js
@@ -1,16 +1,47 @@
 'use strict';
 
 class List {
+    constructor() {
+      this.length = 0;
+    }
+    push(item) {
+      this[this.length++] = item;
+      this.length;
+    } 
+    pop() {
+      this[this.length--];
+      delete this[this.length];
+      return this;
+      }
+    forEach(func) {
+      for(let i=0; i < this.length; i ++){
+        func(this[i]);
+        }
+        return undefined;
+      }
+    }
 
-  constructor() {
-    this.length = 0;
+    
+module.exports =List;
+
+
+
+
+  toArray() {
+
+      let array = [];
+
+       //start at zero, end at length
+
+      for (let i = 0; i < this.length; i++) {
+
+          array[i] = this[i];
+      }
+
+      return array;
+
+
+
   }
 
-  push(item) {
-    this[this.length++] = item;
-    return this.length;
-  }
 
-}
-
-module.exports = List;
